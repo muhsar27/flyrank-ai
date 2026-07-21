b9df72dc72709c4ebef3e2cbfee85969afa9639c
msarumi <muhsar4321@gmail.com>
2026-07-21 16:57:52 +0100

    refactoring

---
+++ CRUD_API/app.js
@@ -0,0 +1,29 @@
+//import http from "http"
+const http = require('http')
+const fs = require('fs')
+const port = 8080;
+
+const server = http.createServer(function (req, res) {
+
+
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    fs.readFile('index.html', function (error, data) {
+        if (error) {
+            res.writeHead(404)
+            res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end()
+    })
+
+})
+
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log('Something went wrong', error)
+    } else {
+        console.log('Makima is listening on port ' + port)
+    }
+})
