e4af4bb910800ef7cef01b354e6b1bff398d55bf
msarumi <muhsar4321@gmail.com>
2026-08-10 16:57:45 +0100

    storing tasks

--- CRUD_API/app.js
+++ CRUD_API/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const path = require('path')
 const app = express()
-
+const taskList = 
+[{id:1,title:"First Task",done:true}]
 const port = 3000;
 
 const swagger = require('swagger-ui-express')
@@ -16,6 +17,11 @@ app.get('/', (req, res) => {
 
 app.get('/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
+})
+
+app.get('/tasks', (req, res) => {
+    
+    res.json(taskList)
 })
 
 app.listen(port, () => {
