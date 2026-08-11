064b3a424bfcc83853472fd31f9a84c61f124929
msarumi <muhsar4321@gmail.com>
2026-08-11 17:06:28 +0100

    handling post requests

--- CRUD_API/app.js
+++ CRUD_API/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const taskList = [{ id: 1, title: "First Task", done: true }];
+const taskList = [
+  { id: 1, title: "First Task", done: true },
+  { id: 2, title: "Second Task", done: false },
+  { id: 3, title: "Problem", done: true },
+];
 const port = 3000;
 
 const swagger = require("swagger-ui-express");
@@ -32,6 +36,26 @@ app.get("/tasks/:id", (req, res) => {
   res.send(task);
 });
 
+app.post("/tasks", (req, res) => {
+    const title = req.body.title; 
+
+    if (!title || title.trim() === ""){
+        return res.status(404).send("Request timed out");
+    }
+    
+    const ids = taskList.map(ids => taskList.id)
+
+    
+    const newTask = []
+
+
+    
+    
+    
+
+    
+  res.send("POST REQUEST SENT");
+});
 app.listen(port, () => {
   console.log(`Makima is listening on ${port}`);
 });
